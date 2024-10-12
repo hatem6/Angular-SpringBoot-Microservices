4adffa585833fda61eb4cd7abb98d1819fbc3113
hatem6 <hatematig25@gmail.com>
2024-10-12 17:13:25 +0100

    addin api gateway-service

---
+++ projet-integration/backend/gateway-service/src/main/java/com/example/gateway_service/config/GatewayConfig.java
@@ -0,0 +1,18 @@
+package com.example.gateway_service.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("agency-service", r -> r.path("/api/agences/**")
+                    .uri("http://localhost:8081/"))
+            .build();
+    }
+}
+
