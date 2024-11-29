5b46e9a13b7b5ab3cdce4c25be20b57f65b1759f
Khouloudmd <khouloud20md@gmail.com>
2024-11-29 11:38:25 +0100

    update:(name = "agency-service", url = "http://localhost:8081")

--- projet-integration/backend/offer-service/src/main/java/com/example/offer_service/client/AgencyClient.java
+++ projet-integration/backend/offer-service/src/main/java/com/example/offer_service/client/AgencyClient.java
@@ -3,9 +3,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.offer_service.dto.Agence;
-@FeignClient(name = "agency-service")
+@FeignClient(name = "agency-service", url = "http://localhost:8081")
 public interface AgencyClient {
     @GetMapping("/api/agences/{id}")
     Agence getAgenceById(@PathVariable("id") Long id);
 }
+
 
